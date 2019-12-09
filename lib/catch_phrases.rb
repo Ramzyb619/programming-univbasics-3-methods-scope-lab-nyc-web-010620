f77b4a3342b1131a9409d3fb938d4f24f11dec0d
Ramzy Badawy <ramzy.badawy@gmail.com>
2019-12-09 22:36:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,24 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = phrase = "It's-a me, Mario!"
   puts phrase
 end
 
-def toadstool
-  puts status
-end
+mario 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# def toadstool
+#   puts status
+# end
